4a5e55223086b9a64ebb1aebfffb357b48a0c2c5
lihongyang0453 <32625523+lihongyang0453@users.noreply.github.com>
2021-05-15 17:00:29 +0800

    注释代码

--- controllers/MainController.go
+++ controllers/MainController.go
@@ -1,9 +1,6 @@
 package controllers
 
 import (
-	service "GoMVC/services"
-	logHelper "GoMVC/utils/LogHelper"
-
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -15,13 +12,13 @@ func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 
-	userService := new(service.UserService)
-	var totalCount int
-	list := userService.GetList(1, 10, nil, "", &totalCount)
-	if len(list) > 0 {
-		go logHelper.LogInfo("go")
-		logHelper.LogInfo("normal")
-	}
+	//userService := new(service.UserService)
+	//var totalCount int
+	//list := userService.GetList(1, 10, nil, "", &totalCount)
+	// if len(list) > 0 {
+	// 	go logHelper.LogInfo("go")
+	// 	logHelper.LogInfo("normal")
+	// }
 	// CacheHelper.Add("name", "lhy")
 	// CacheHelper.AddNX("name", "lhy")
 	// model := userService.FindById(20)
